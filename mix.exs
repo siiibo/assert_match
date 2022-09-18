8509739f03c0fe91635907cc3a40cd551543d1ab
Yu Matsuzawa <ymtszw@gmail.com>
2022-09-18 05:05:39 +0000

    feat: write package related info

--- mix.exs
+++ mix.exs
@@ -4,9 +4,23 @@ defmodule AssertMatch.MixProject do
   def project do
     [
       app: :assert_match,
+      description:
+        "Leverages pattern matching & pipeline in Elixir tests by pipe-friendly `assert_match/2`",
       version: "1.0.0",
       elixir: "~> 1.10",
+      package: package(),
       deps: []
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: [
+        "Apache-2.0"
+      ],
+      links: %{
+        "GitHub" => "https://github.com/siiibo/assert_match"
+      }
     ]
   end
 end
