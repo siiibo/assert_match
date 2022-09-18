3ac12da127ad162458727b3abaf91ec3f0817ac4
Yu Matsuzawa <ymtszw@gmail.com>
2022-09-18 05:09:24 +0000

    ci: setup release tools

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Release script
+#
+# Usage:
+# 1. Set new version in mix.exs
+# 2. Commit and push
+# 3. Run ./release.sh
+# 4. Let .github/workflows/publish.yml to publish
+
+version=$(mix run -e 'IO.puts(Mix.Project.config[:version])')
+git tag "v$version"
+git push "v$version"
