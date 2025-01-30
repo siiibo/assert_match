b36496ef4095852fd69aa0e17685ebea170c1a44
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2025-01-30 21:10:06 +0900

    feat: bump as minor version change due to behavior change

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule AssertMatch.MixProject do
       app: :assert_match,
       description:
         "Leverages pattern matching & pipeline in Elixir tests by pipe-friendly `assert_match/2`",
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.10",
       package: package(),
       deps: [{:ex_doc, "~> 0.27", only: :dev, runtime: false}]
